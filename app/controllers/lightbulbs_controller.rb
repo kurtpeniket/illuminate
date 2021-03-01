98289b8f20811f39812c531fea52fae509881a6d
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-03-01 17:45:26 +0000

    Lightbulb controller generated, and show/index routes added

---
+++ app/controllers/lightbulbs_controller.rb
@@ -0,0 +1,8 @@
+class LightbulbsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/lightbulbs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LightbulbsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
