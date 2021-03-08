4d328a772843192b07d7f75cd53c51c0f9be3a4c
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-03-08 11:42:14 +0000

    Favourites create and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_113211) do
+ActiveRecord::Schema.define(version: 2021_03_08_094713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "lightbulb_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lightbulb_id"], name: "index_favourites_on_lightbulb_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
 
   create_table "lightbulbs", force: :cascade do |t|
     t.string "bulb_type"
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_113211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "lightbulbs"
+  add_foreign_key "favourites", "users"
   add_foreign_key "orders", "lightbulbs"
   add_foreign_key "orders", "shops"
 end
