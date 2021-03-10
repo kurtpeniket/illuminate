f649d4b39c948882c9f715683074fade2e8900df
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-03-10 12:41:16 +0000

    Update method added to favourites controller - JS not calling it yet though

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,10 @@
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :lightbulb
+  before_create :set_order
+
+  def set_order
+    new_order = self.user.favourites.length + 1
+    self.order = new_order
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_110828) do
+ActiveRecord::Schema.define(version: 2021_03_10_105255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_110828) do
     t.bigint "lightbulb_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "order"
     t.index ["lightbulb_id"], name: "index_favourites_on_lightbulb_id"
     t.index ["user_id"], name: "index_favourites_on_user_id"
   end
