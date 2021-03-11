fb5c74b78c122ccdc18fb18ebf97609c57a43be5
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-11 18:08:31 +0000

    loader fixes

--- app/javascript/components/loader.js
+++ app/javascript/components/loader.js
@@ -1,13 +1,22 @@
 export const loader = () => {
+  const btn = document.querySelector('#search-btn')
   const preloader = document.querySelector('.preloader');
-  const fadeEffect = setInterval(() => {
-    if (!preloader.style.opacity) {
-      preloader.style.opacity = 1;
-    }
-    if (preloader.style.opacity > 0) {
-      preloader.style.opacity -= 0.7;
-    } else {
-      clearInterval(fadeEffect);
-    }
-  }, 1500);
+  
+  // const fadeEffect = setInterval(() => {
+    //   if (!preloader.style.opacity) {
+      //     preloader.style.opacity = 1;
+      //   }
+      //   if (preloader.style.opacity > 0) {
+        //     preloader.style.opacity -= 0.7;
+        //   } else {
+          //     clearInterval(fadeEffect);
+          //   }
+          // }, 500);
+  if (preloader) {
+    // window.addEventListener('load', e => {
+      setTimeout(function(){
+        preloader.classList.add('preloader-invisible')
+      }, 2000)
+    // })
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+  get 'lightbulbs/load', to: 'lightbulbs#load'
   get 'lightbulbs/search', to: 'lightbulbs#search'
   resources :lightbulbs, only: [:index, :show] do
       resources :favourites, only: [:create]
