baf5b895450766a52e8198315f4d75a71486aa26
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-11 12:32:07 +0000

    ocr functionality and controller paths defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+  get 'lightbulbs/search', to: 'lightbulbs#search'
   resources :lightbulbs, only: [:index, :show] do
       resources :favourites, only: [:create]
     end
   resources :favourites
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
