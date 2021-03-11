01759d668a6a4b0b0e204eda937279369a0b0271
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-11 09:39:57 +0000

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
   def default_url_options
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
-  config.session_store :cache_store
+  # config.session_store :cache_store
 
 
   # In the development environment your application's code is reloaded on
